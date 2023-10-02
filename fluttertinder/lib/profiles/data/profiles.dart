b6ec05ec13d8a5c750ac536de1a854fda3fefb54
KyanPepper <kyankks1@gmail.com>
2023-10-01 23:46:10 -0700

    edited pubsped to include pictures as assets

--- fluttertinder/lib/profiles/data/profiles.dart
+++ fluttertinder/lib/profiles/data/profiles.dart
@@ -3,12 +3,14 @@ class TinderProfile {
   final String description;
   final String starsign;
   final String hobbies;
+  final String image;
 
   TinderProfile(
       {required this.name,
       required this.description,
       required this.starsign,
-      required this.hobbies});
+      required this.hobbies,
+      this.image = "images/defPic.png"});
 }
 
 final List<TinderProfile> profiles = [
@@ -17,23 +19,27 @@ final List<TinderProfile> profiles = [
     description: 'I love hiking and reading.',
     starsign: 'Aries',
     hobbies: 'Fishing',
+    image: "images/adam.png"
   ),
   TinderProfile(
     name: 'Peter',
     description: 'I enjoy traveling and cooking.',
     starsign: 'Leo',
     hobbies: 'Painting',
+    image: "images/peter.png"
   ),
   TinderProfile(
     name: 'Ben',
     description: 'I love eating worms and other bacteria',
     starsign: 'Capricorn',
     hobbies: 'Mountain Goat Hunting',
+    image: "images/ben.png"
   ),
   TinderProfile(
     name: 'Kyan',
     description: 'I am a programmer',
     starsign: 'Cancer',
     hobbies: 'Coding in flutter',
+    image: "images/Saltkyan.png"
   )
 ];

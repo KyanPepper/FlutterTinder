374a239f341a591c6a254701c1ba1986efe8221b
KyanPepper <kyankks1@gmail.com>
2023-10-01 22:00:46 -0700

    Added addprofile form

--- fluttertinder/lib/profiles/application/home.dart
+++ fluttertinder/lib/profiles/application/home.dart
@@ -38,3 +38,5 @@ class CreateProfileButton extends StatelessWidget {
         ),
       );
 }
+
+
--- fluttertinder/lib/profiles/application/tinder_profile.dart
+++ fluttertinder/lib/profiles/application/tinder_profile.dart
@@ -14,3 +14,42 @@ class TinderProfileDisplay extends StatelessWidget {
     );
   }
 }
+
+// Define a custom Form widget.
+class ProfileaddForm extends StatefulWidget {
+  const ProfileaddForm({super.key});
+
+  @override
+  FormState createState() {
+    return FormState();
+  }
+}
+
+// This class holds data related to the form.
+class FormState extends State<ProfileaddForm> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
+  TextEditingController hobbiesController = TextEditingController();
+  TextEditingController signController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          const Text("Name"),
+          TextFormField(controller: usernameController,),
+          const Text("Bio"),
+          TextFormField(controller: bioController,),
+          const Text("Hobbies"),
+          TextFormField(controller: hobbiesController,),
+          const Text("Star Sign"),
+          TextFormField(controller: signController,),
+          // Add TextFormFields and ElevatedButton here.
+        ],
+      ),
+    );
+  }
+}
